184e29194b1ee610238e54aa64e438937bd2226f
dyalasaadeh <60358200+dyalasaadeh@users.noreply.github.com>
2020-02-08 11:28:22 +0200

    Update first.java
    call adder func for main

--- src/myp/first.java
+++ src/myp/first.java
@@ -4,6 +4,7 @@ public class first {
 
 		public static void main(String[] args) {
 			System.out.print("His Byes Guys");
+			int sum = adder(10,5,9);
 		}
 		public int adder (int x, int y,int z) {
 			return x+y+z;

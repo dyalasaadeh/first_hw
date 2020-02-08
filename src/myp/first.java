8af1275be69180ec0e9757d33fd297befb840dfa
USER <USER@3578-i7-SSD-8th>
2020-02-08 11:23:08 +0200

    apply an adder for two number

---
+++ src/myp/first.java
@@ -0,0 +1,14 @@
+package myp;
+
+public class first {
+
+		public static void main(String[] args) {
+			System.out.print("His Byes Guys");
+		}
+		public int adder (int x, int y) {
+			return x+y;
+			
+		}
+	}
+
+

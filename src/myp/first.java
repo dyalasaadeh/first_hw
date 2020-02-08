157012425271e552172b079d6e4a274de34c791c
USER <USER@3578-i7-SSD-8th>
2020-02-08 11:26:15 +0200

    improve adder func to be able to add three numbers

--- src/myp/first.java
+++ src/myp/first.java
@@ -5,8 +5,8 @@ public class first {
 		public static void main(String[] args) {
 			System.out.print("His Byes Guys");
 		}
-		public int adder (int x, int y) {
-			return x+y;
+		public int adder (int x, int y,int z) {
+			return x+y+z;
 			
 		}
 	}
